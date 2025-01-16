4bf3f8481db7a6be57a6071d17f0505d95bbed9e
Md Sumon <109859876+Mdsumon743@users.noreply.github.com>
2025-01-16 17:11:05 +0600

    .........

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
