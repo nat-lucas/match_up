227287c81cbf2af347520d1399ce9d7708392c29
Md Sumon <109859876+Mdsumon743@users.noreply.github.com>
2025-04-19 16:39:45 +0600

    done

--- lib/core/validator/validator.dart
+++ lib/core/validator/validator.dart
@@ -7,26 +7,6 @@ String? validatePassword(String? value) {
     return 'Password must be at least 6 characters long';
   }
 
-  final upperCasePattern = RegExp(r'[A-Z]');
-  if (!upperCasePattern.hasMatch(value)) {
-    return 'Password must contain at least one uppercase letter';
-  }
-
-  final lowerCasePattern = RegExp(r'[a-z]');
-  if (!lowerCasePattern.hasMatch(value)) {
-    return 'Password must contain at least one lowercase letter';
-  }
-
-  final numberPattern = RegExp(r'\d');
-  if (!numberPattern.hasMatch(value)) {
-    return 'Password must contain at least one number';
-  }
-
-  final specialCharPattern = RegExp(r'[!@#\$&*~]');
-  if (!specialCharPattern.hasMatch(value)) {
-    return 'Password must contain at least one special character (e.g., !@#\$&*~)';
-  }
-
   return null;
 }
 
